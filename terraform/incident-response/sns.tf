88512a7303844c309dbeedc88c966d0fb5d2e75f
essiewakukha <esthermwakukha@gmail.com>
2026-08-23 01:21:09 +0300

    adding sns topic

--- terraform/incident-response/sns.tf
+++ terraform/incident-response/sns.tf
@@ -1,34 +1,9 @@
-
 resource "aws_sns_topic" "security_alerts" {
-  name              = "security-team-alerts"
-  kms_master_key_id = var.findings_kms_key_arn
+  name = "security-team-alerts"
 }
 
 resource "aws_sns_topic_subscription" "security_email" {
   topic_arn = aws_sns_topic.security_alerts.arn
   protocol  = "email"
   endpoint  = var.security_alerts_email
-}
-
-resource "aws_sns_topic_policy" "security_alerts" {
-  arn = aws_sns_topic.security_alerts.arn
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "AllowStepFunctionsPublish"
-        Effect    = "Allow"
-        Principal = { Service = "states.amazonaws.com" }
-        Action    = "sns:Publish"
-        Resource  = aws_sns_topic.security_alerts.arn
-      },
-      {
-        Sid       = "AllowEventBridgePublish"
-        Effect    = "Allow"
-        Principal = { Service = "events.amazonaws.com" }
-        Action    = "sns:Publish"
-        Resource  = aws_sns_topic.security_alerts.arn
-      }
-    ]
-  })
 }
